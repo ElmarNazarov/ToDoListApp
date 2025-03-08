87c42b19f5a3092946bae4b7a0f42c3ab5341b39
Elmar Nazarov <ElmarNazarov>
2025-03-08 09:21:33 +0400

    Editing a style

--- frontend/src/app/home/layout.jsx
+++ frontend/src/app/home/layout.jsx
@@ -10,9 +10,9 @@ export default function HomeLayout({ children }) {
     <UserProvider>
       <ProtectedRoute>
         <div className="flex flex-col md:flex-row h-full md:h-screen min-h-0 overflow-hidden">
-          <div className="flex-1 flex flex-col">
+          <div className="flex-1 flex flex-col bg-gray-100">
             <Header />
-            <main className="flex-1 p-4 md:p-6 bg-gray-100 overflow-y-auto h-full">
+            <main className="flex-1 p-4 md:p-6 overflow-y-auto h-full">
               {children}
             </main>
           </div>
